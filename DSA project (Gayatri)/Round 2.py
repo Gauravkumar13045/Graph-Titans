3a7c9053354b4c5f2ab32804fb72849a5fd41936
Gayatri <gayatrixreal@gmail.com>
2025-12-09 13:20:33 +0530

    Implement missingNumber method in Solution class
    Added a new method to find missing numbers in the list.

--- DSA project (Gayatri)/Round 2.py
+++ DSA project (Gayatri)/Round 2.py
@@ -8,3 +8,14 @@ class Solution:
                 return i 
             left_sum += nums[i]
         return -1
+
+# Missing numbers
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n= len(nums)
+        sums = n*(n+1)//2
+        realsum = sum(nums)
+        return sums - realsum 
+
+        
