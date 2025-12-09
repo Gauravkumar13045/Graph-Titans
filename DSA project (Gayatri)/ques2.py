7b606081cfa4bc7cb28d80bffda039910d3643cd
Gayatri <gayatrixreal@gmail.com>
2025-12-09 13:47:01 +0530

    Add missing number solution in ques2.py
    Implement a solution to find the missing number in a list.

---
+++ DSA project (Gayatri)/ques2.py
@@ -0,0 +1,11 @@
+
+# Missing numbers
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n= len(nums)
+        sums = n*(n+1)//2
+        realsum = sum(nums)
+        return sums - realsum 
+
+        
